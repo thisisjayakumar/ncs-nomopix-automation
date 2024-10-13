9aadcd96f1555d35a64dd964d2871a8f87acc280
thisisjayakumar <jayagma032@gmail.com>
2024-10-13 13:34:24 +0530

    changes webdiver

--- selenium_headless_browser/utils.py
+++ selenium_headless_browser/utils.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 def setup_driver():
