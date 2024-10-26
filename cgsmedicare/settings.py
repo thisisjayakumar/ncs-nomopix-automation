261ed9b0a2e63c9c99f0dd24c94144abf92813e2
thisisjayakumar <jayagma032@gmail.com>
2024-10-26 19:53:36 +0530

    database changes

--- cgsmedicare/settings.py
+++ cgsmedicare/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'cgsmedicare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 AUTH_USER_MODEL = 'users.User'
