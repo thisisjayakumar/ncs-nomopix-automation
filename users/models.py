a593bd98a3e1fc674d98db48107bdca7b8e0eea7
Jayakumar N <jayagma032@gmail.com>
2024-10-29 16:12:40 +0530

    migration files

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True) 
     created_at = models.DateTimeField(auto_now_add=True)
